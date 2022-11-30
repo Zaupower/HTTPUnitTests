3f605fe85ed0897d4cda0ca2beea0e14845f79d8
Zaupower <56695737+Zaupower@users.noreply.github.com>
2022-11-30 09:47:30 +0000

    Invalid user test

--- RegisterTests/RegisterNewUserTests.cs
+++ RegisterTests/RegisterNewUserTests.cs
@@ -22,28 +22,46 @@ namespace UserServiceAPITests.RegisterTests
         }
 
         [Test]
-        public async Task ValidUSer_RegisterNewUSer_ResponseStatusIsOk()
+        public async Task ValidUser_RegisterNewUSer_ResponseStatusIsOk()
         {
             
-
+            //Precondition
             CreateUserRequest request = new CreateUserRequest
             {
                 firstName = "fisrtNameTest1",
                 lastName = "lastNameTest1"
             };
 
+            //Action
             HttpResponse<int> response = await _serviceProvider.CreateUser(request);
             
-            //Assert.Greater(int.Parse( response.Body.id), 0);
+            Assert.Greater( response.Body,0);
             Assert.AreEqual(HttpStatusCode.OK, response.HttpStatusCode);
-
         }
 
         [Test]
-        public async Task ValidUSer_DeleteUser_ResponseStatusIsOk()
+        public async Task InvalidUser_RegisterNewUSer_ResponseStatusIsOk()
+        {
+
+            //Precondition
+            CreateUserRequest request = new CreateUserRequest
+            {
+                firstName = null,
+                lastName = null
+            };
+
+            //Action
+            HttpResponse<int> response = await _serviceProvider.CreateUser(request);
+
+            //Assert
+            Assert.AreEqual(response.Content, "An error occurred while saving the entity changes. See the inner exception for details.");
+            Assert.AreEqual(HttpStatusCode.InternalServerError, response.HttpStatusCode);
+        }
+
+        [Test]
+        public async Task ValidUser_DeleteUser_ResponseStatusIsOk()
         {
             //Precondition
-
             CreateUserRequest request = new CreateUserRequest
             {
                 firstName = "fisrt_name_test1",
@@ -52,16 +70,26 @@ namespace UserServiceAPITests.RegisterTests
 
             HttpResponse<int> createUserResponse = await _serviceProvider.CreateUser(request);
             int userId =  createUserResponse.Body;
+            
             //Action
-
             var deleteResponse = await _serviceProvider.DeleteUser(userId);
 
+            //Assert
+            Assert.AreEqual(HttpStatusCode.OK, deleteResponse.HttpStatusCode);
+        }
+
+        [Test]
+        public async Task InvalidUser_DeleteUser_ResponseStatusInternalServerError()
+        {
+            //Precondiction
+            int userId = 0;
+            
+            //Action
+            var deleteResponse = await _serviceProvider.DeleteUser(userId);
 
             //Assert
-            //Assert.Greater(createUserResponse.id, 0);
-            //Assert.AreEqual(HttpStatusCode.OK, response.HttpStatusCode);
-            Assert.AreEqual(HttpStatusCode.OK, deleteResponse.HttpStatusCode);
-
+            Assert.AreEqual(deleteResponse.Content, "Sequence contains no elements");
+            Assert.AreEqual(HttpStatusCode.InternalServerError, deleteResponse.HttpStatusCode);
         }
     }
 }
