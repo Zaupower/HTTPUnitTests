9f64743dce5257867de2c0e363a2c0e78da59127
Zaupower <56695737+Zaupower@users.noreply.github.com>
2022-12-23 15:11:39 +0000

    GetBalance Tests Update

--- WalletServiceAPITests/Scenarios/WalletService/BalanceTests.cs
+++ WalletServiceAPITests/Scenarios/WalletService/BalanceTests.cs
@@ -1,5 +1,10 @@
 ﻿using NUnit.Framework;
+using System;
 using System.Net;
+using UserServiceAPITests.Helper;
+using UserServiceAPITests.Models.Requests.UserService;
+using UserServiceAPITests.ServiceProvider;
+using WalletServiceAPITests.Models.Responses.Base;
 using WalletServiceAPITests.ServiceProvider;
 
 namespace WalletServiceAPITests.Scenarios.WalletService
@@ -8,6 +13,11 @@ namespace WalletServiceAPITests.Scenarios.WalletService
     public class BalanceTests
     {
         private WalletServiceServiceProvider _walletServiceProvider = WalletServiceServiceProvider.Instance;
+        private UserServiceServiceProvider _userServiceProvider = UserServiceServiceProvider.Instance;
+
+
+        private GenerateUsersRequest _generateUsersRequest = GenerateUsersRequest.Instance;
+
         private WalletServiceAPITests.TestDataObserver _observerCharge;
         private WalletServiceAPITests.TestDataObserverDeleteAction _observerRevert;
 
@@ -19,6 +29,9 @@ namespace WalletServiceAPITests.Scenarios.WalletService
 
             _walletServiceProvider.Subscribe(_observerCharge);
             _walletServiceProvider.SubscribeRevert(_observerRevert);
+
+
+
         }
         [OneTimeTearDown]
         public async Task teardown()
@@ -55,6 +68,25 @@ namespace WalletServiceAPITests.Scenarios.WalletService
         {
             //Precondition
             double expectedBody = 0;
+            //Action
+            var response = await _walletServiceProvider.GetBalance(userId);
+
+            //Assert
+            Assert.AreEqual(HttpStatusCode.InternalServerError, response.HttpStatusCode);
+            Assert.AreEqual(expectedBody, response.Body);
+            Assert.AreEqual(response.Content, "not active user");
+        }
+
+        [Test]
+        public async Task GetBalance_NewUser_ResponseStatusIsOk()
+        {
+            //Precondition
+            double expectedBody = 0;
+            CreateUserRequest newUSer = _generateUsersRequest.generateUser();
+
+            var createUserresponse = await _userServiceProvider.CreateUser(newUSer);
+
+            int userId = createUserresponse.Body;
             //Action
             var response = await _walletServiceProvider.GetBalance(userId);
 
