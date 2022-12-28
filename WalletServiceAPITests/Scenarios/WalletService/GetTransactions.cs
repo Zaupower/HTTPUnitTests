e4492107a6f7637f4dd1c031571e785398eb882b
Zaupower <56695737+Zaupower@users.noreply.github.com>
2022-12-28 11:15:58 +0000

    Add GetTransactions Tests

--- WalletServiceAPITests/Scenarios/WalletService/GetTransactions.cs
+++ WalletServiceAPITests/Scenarios/WalletService/GetTransactions.cs
@@ -2,6 +2,7 @@
 using NUnit.Framework.Constraints;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.Metrics;
 using System.Linq;
 using System.Net;
 using System.Text;
@@ -147,7 +148,7 @@ namespace WalletServiceAPITests.Scenarios.WalletService
             Assert.AreEqual(transactions.Count, 1);
             //Assert.AreEqual(expedctedTrtansactionStatus, transaction.status);
         }
-
+        
         [Test]
         public async Task GetTransactions_SeveralTransactions_CorrectStatus([Values(3, 30, 55)] int numberOfTransactions)
         {
@@ -170,7 +171,98 @@ namespace WalletServiceAPITests.Scenarios.WalletService
 
             //Assert
             Assert.AreEqual(numberOfTransactions, transactions.Count );
-            //Assert.AreEqual(expedctedTrtansactionStatus, transaction.status);
+        }
+
+        [Test]
+        public async Task GetTransactions_CheckSortingByTime_CorrectOrder([Values(3, 30, 55)] int numberOfTransactions)
+        {
+            Random random = new Random();
+            //Precondition
+            int userId = await CreateAndVerifyUser();
+            ChargeModel chargeModel = new ChargeModel();
+            chargeModel.userId = userId;
+
+            //Make transaction
+            for (int i = 1; i < numberOfTransactions; i++)
+            {
+                chargeModel.amount = random.NextDouble(1.00, 50000.00);
+                await _walletServiceProvider.PostCharge(chargeModel);
+                Thread.Sleep(TimeSpan.FromSeconds(0.01));
+            }
+            await _walletServiceProvider.PostCharge(chargeModel);
+            //Action
+            var getTransactionsResponse = await _walletServiceProvider.GetTransactions(userId);
+            List<GetTransactionModel> transactions = getTransactionsResponse.Body;
+
+            //Assert
+            Assert.That(transactions, Is.Ordered.By("time").Descending);
+        }
+
+        [Test]
+        public async Task GetTransactions_CheckAllFieldsAreNotNull_ValuesInRange([Values(3, 15, 20)] int numberOfTransactions)
+        {
+            Random random = new Random();
+            //Precondition
+            int userId = await CreateAndVerifyUser();
+            ChargeModel chargeModel = new ChargeModel();
+            chargeModel.userId = userId;
+
+            //Make transaction
+            for (int i = 1; i < numberOfTransactions; i++)
+            {
+                chargeModel.amount = random.NextDouble(0.01, 9999999.99);
+                await _walletServiceProvider.PostCharge(chargeModel);
+                Thread.Sleep(TimeSpan.FromSeconds(0.01));
+            }
+            await _walletServiceProvider.PostCharge(chargeModel);
+            //Action
+            var getTransactionsResponse = await _walletServiceProvider.GetTransactions(userId);
+            List<GetTransactionModel> transactions = getTransactionsResponse.Body;
+
+            //Assert
+            foreach(var transaction in transactions)
+            {
+                Assert.That(transaction.amount, Is.GreaterThan(0));
+                Assert.That(transaction.time, Is.LessThanOrEqualTo(DateTime.Now));
+                Assert.That(transaction.status, Is.GreaterThan(-2));
+                Assert.That(transaction.baseTransactionId, Is.EqualTo(null));
+            }
+        }
+
+        [Test]
+        public async Task GetTransactions_MakeGetTransactionsRequestAfterRevert_ValuesInRange()
+        {
+            Random random = new Random();
+            //Precondition
+            int userId = await CreateAndVerifyUser();
+            ChargeModel chargeModel = new ChargeModel();
+            chargeModel.userId = userId;
+            chargeModel.amount = random.NextDouble(0.01, 9999999.99);
+            
+            var transactionResult = await _walletServiceProvider.PostCharge(chargeModel);
+            
+            Thread.Sleep(TimeSpan.FromSeconds(0.01));
+            
+            await _walletServiceProvider.RevertTransaction(transactionResult.Body);
+            //Action
+
+            var getTransactionsResponse = await _walletServiceProvider.GetTransactions(userId);
+            List<GetTransactionModel> transactions = getTransactionsResponse.Body;
+
+            //Assert
+
+            GetTransactionModel firstTransaction = transactions[1];
+            GetTransactionModel revertTransaction = transactions[0];
+            
+                Assert.That(firstTransaction.amount, Is.GreaterThan(0));
+                Assert.That(firstTransaction.time, Is.LessThanOrEqualTo(DateTime.Now));
+                Assert.That(firstTransaction.status, Is.GreaterThan(-2));
+                Assert.That(firstTransaction.baseTransactionId, Is.EqualTo(null));
+
+                Assert.That(revertTransaction.amount, Is.GreaterThanOrEqualTo(-9999999.99));
+                Assert.That(revertTransaction.time, Is.LessThanOrEqualTo(DateTime.Now));
+                Assert.That(revertTransaction.status, Is.GreaterThan(-2));
+                Assert.That(revertTransaction.baseTransactionId, Is.EqualTo(transactionResult.Body));            
         }
 
     }
