2790aa0a95d471b38ca3882a9c69e2e4ccf955dc
Zaupower <56695737+Zaupower@users.noreply.github.com>
2023-01-12 16:13:10 +0000

    Add First spcflow test to create new user

---
+++ UserServiceAPITests/SpecFlowTests/Scenarios/UserService.feature
@@ -0,0 +1,6 @@
+﻿Feature: UserService
+
+Scenario: Register new valid User - Server Response Status Is Ok
+	Given User registration values are valid
+	When Register User
+	Then Create User Request Status is 'OK'
---
+++ UserServiceAPITests/SpecFlowTests/Steps/UserServiceSteps.cs
@@ -0,0 +1,40 @@
+﻿using CommonLogic.Models.Responses.Base;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+using System;
+using System.Net;
+using TechTalk.SpecFlow;
+using UserServiceAPITests.Helper;
+using UserServiceAPITests.Models.Requests.UserService;
+using UserServiceAPITests.ServiceProvider;
+
+namespace UserServiceAPITests.SpecFlowTests.Steps
+{
+    [Binding]
+    public class UserServiceSteps
+    {
+        private CreateUserRequest user;
+        private HttpResponse<int> response;
+        private UserServiceServiceProvider _serviceProvider = UserServiceServiceProvider.Instance;
+        
+        [Given("User registration values are valid")]//Precondition
+        public void CreateUser()
+        {
+            user = GenerateUsersRequest.Instance.generateUser();
+        }
+
+        [When("Register User")]//Action        
+        public async Task RegisterUser()
+        {
+            response = await _serviceProvider.CreateUser(user);
+            //Assert
+        }
+
+        [Then("Create User Request Status is '(.*)'")]//Assert
+        public void ThenCreateUserRequestStatusIsOK(HttpStatusCode expectedStatusCode)
+        {
+            Assert.AreEqual(expectedStatusCode, response.HttpStatusCode);
+        }
+
+    }
+}
